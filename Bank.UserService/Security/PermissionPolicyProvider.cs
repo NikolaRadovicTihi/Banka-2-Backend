0edbd11fce4f43d5fc51707eb12a6de471584f8c
NikolaRadovicTihi <114955835+NikolaRadovicTihi@users.noreply.github.com>
2025-04-06 16:58:40 +0200

    Fix imports

--- Bank.UserService/Security/PermissionPolicyProvider.cs
+++ Bank.UserService/Security/PermissionPolicyProvider.cs
@@ -7,7 +7,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-using Bank.UserService.Authorization;
+using Bank.UserService.Security;
 
 namespace Bank.UserService.Security
 {
